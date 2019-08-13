dd1852e22de87a76d5d9448e770048b679c97305
Thanh Vuong <thanhvg@gmail.com>
2019-08-12 21:10:51 -0600

    doc doc

--- howdoyou.el
+++ howdoyou.el
@@ -22,17 +22,19 @@
 
 ;;; Commentary:
 
-;; This package is inspired by howdoi python and howdoi Emacs package. it
-;; searches your query all stackoverflow and it's sisters' sites. They are:
-;; stackoverflow.com, stackexchange.com, superuser.com, serverfault.com and
-;; askubunu.com. The result is then showed in an `org-mode' buffer. For each
-;; result, the question and three answers were showed, but they are collapsed by
-;; default except the first answer. As this package uses Google to get the links,
-;; for each query there will be a dozen of links, the fist link will be used,
-;; but then use can go to next link and previous link. The author believes that
-;; when searching for solutions it is important for users to read both questions
-;; and answers, so no "quick look" features such as code only view or code
-;; completion are provided.
+;; This package is inspired by python howdoi (https://github.com/gleitz/howdoi)
+;; and howdoi Emacs package (https://github.com/lockie/emacs-howdoi and
+;; https://github.com/atykhonov/emacs-howdoi). it searches your query all
+;; accross stackoverflow and it's sisters' sites. They are: stackoverflow.com,
+;; stackexchange.com, superuser.com, serverfault.com and askubunu.com. The
+;; result is then showed in an `org-mode' buffer. For each result, the question
+;; and three answers were showed, but they are collapsed by default except the
+;; first answer. As this package uses Google to get the links, for each query
+;; there will be a dozen of links, the fist link will be used, but then use can
+;; go to next link and previous link. The author believes that when searching
+;; for solutions it is important for users to read both questions and answers,
+;; so no "quick look" features such as code only view or code completion are
+;; provided.
 
 ;;; Dependencies
 ;; `promise' and `request' are required.
