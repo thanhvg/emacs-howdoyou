6fcb95df9c8d6cd8099315008c6673e84086501b
Thanh Vuong <thanhvg@gmail.com>
2020-08-05 11:39:18 -0600

    typo on comment

--- howdoyou.el
+++ howdoyou.el
@@ -23,17 +23,16 @@
 ;;; Commentary:
 ;; This package is inspired by python howdoi (https://github.com/gleitz/howdoi)
 ;; and howdoi Emacs package (https://github.com/lockie/emacs-howdoi and
-;; https://github.com/atykhonov/emacs-howdoi). it searches your query all
-;; across stackoverflow and its sisters' sites. They are: stackoverflow.com,
+;; https://github.com/atykhonov/emacs-howdoi). it searches your query all across
+;; stackoverflow and its sisters' sites. They are: stackoverflow.com,
 ;; stackexchange.com, superuser.com, serverfault.com and askubuntu.com. The
 ;; result is then showed in an `org-mode' buffer. For each result, the question
 ;; and three answers were showed, but they are collapsed by default except the
 ;; first answer. As this package uses Google to get the links, for each query
-;; there will be a dozen of links, the fist link will be used, but then use can
-;; go to next link and previous link. The author believes that when searching
-;; for solutions it is important for users to read both questions and answers,
-;; so no "quick look" features such as code only view or code completion are
-;; provided.
+;; there will be a dozen of links, the fist link will be used, users can go
+;; through these links. The author believes that when searching for solutions it
+;; is important for users to read both questions and answers, so no "quick look"
+;; features such as code only view or code completion are provided.
 
 ;;; Dependencies
 ;; `promise' and `request' are required.
